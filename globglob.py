cbfd12ce91e4583161545e9331fcf88d9421b32b
Jenny <jenny.saukko@hotmail.com>
2024-09-18 15:32:26 +0300

    Lisätty trollausta gg

--- globglob.py
+++ globglob.py
@@ -1,5 +1,10 @@
 import webbrowser
+import random
 
-url = "https://www.youtube.com/watch?v=dtER80sOjX4"
+lista = ["https://www.youtube.com/watch?v=dtER80sOjX4",
+        "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        "https://www.youtube.com/watch?v=PKQPey6L42M"]
+
+url = (random.choice(lista))
 
 webbrowser.open(url)
